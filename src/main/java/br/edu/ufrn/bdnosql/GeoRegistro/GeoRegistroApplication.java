d487d194b6a8219863ecf5020ec5497b161fa18d
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2026-06-20 11:44:34 -0300

    Esqueleto do projeto

---
+++ src/main/java/br/edu/ufrn/bdnosql/GeoRegistro/GeoRegistroApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.bdnosql.GeoRegistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoRegistroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/bdnosql/GeoRegistro/GeoRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.bdnosql.GeoRegistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
