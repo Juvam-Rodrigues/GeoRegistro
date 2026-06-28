613f677a7fd223eaa788853d2d07eef62a210c8b
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2026-06-27 23:07:15 -0300

    Possibilitando requisição externa através do corsconfig.

--- src/main/java/br/edu/ufrn/bdnosql/GeoRegistro/config/CorsConfig.java
+++ src/main/java/br/edu/ufrn/bdnosql/GeoRegistro/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package br.edu.ufrn.bdnosql.GeoRegistro.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
